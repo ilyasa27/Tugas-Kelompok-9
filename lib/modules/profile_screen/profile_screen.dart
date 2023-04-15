9fb8359a56abb90cb243de97a60c48e480e3432f
rafinurajiz <rafinurajiz19@gmail.com>
2023-04-15 21:51:35 +0700

    upgrade profile

---
+++ lib/modules/profile_screen/profile_screen.dart
@@ -0,0 +1,245 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../models/user.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key, required this.user});
+  final User user;
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneNumberController = TextEditingController();
+  bool isVisiblePassword = false;
+
+  @override
+  void initState() {
+    if (widget.user.name != "") {
+      nameController.text = widget.user.name;
+      emailController.text = widget.user.email;
+      phoneNumberController.text = widget.user.phoneNumber!;
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).padding.top,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+            ),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    GoRouter.of(context).pop();
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.red[200],
+                    ),
+                    child: const Icon(
+                      Icons.chevron_left_rounded,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Expanded(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    child: TextFormField(
+                      controller: nameController,
+                      keyboardType: TextInputType.name,
+                      textInputAction: TextInputAction.search,
+                      decoration: InputDecoration(
+                        suffixIcon: const Icon(Icons.person_rounded),
+                        label: const Text("Nama"),
+                        isDense: true,
+                        hintText: "Nama Anda",
+                        fillColor: Colors.red[100],
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == "") {
+                          return "Nama Wajib diisi";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    child: TextFormField(
+                      controller: _passwordController,
+                      obscureText: !isVisiblePassword,
+                      keyboardType: TextInputType.name,
+                      textInputAction: TextInputAction.done,
+                      decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isVisiblePassword = !isVisiblePassword;
+                            });
+                          },
+                          icon: Icon(
+                            isVisiblePassword == false
+                                ? Icons.visibility_rounded
+                                : Icons.visibility_off_rounded,
+                          ),
+                        ),
+                        label: const Text("Password"),
+                        isDense: true,
+                        fillColor: Colors.red[100],
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(30)),
+                      ),
+                      validator: (value) {
+                        if (value == "" || value == null) {
+                          return "Password Wajib diisi";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    child: TextFormField(
+                      controller: emailController,
+                      keyboardType: TextInputType.name,
+                      textInputAction: TextInputAction.search,
+                      decoration: InputDecoration(
+                        suffixIcon: const Icon(Icons.mail_rounded),
+                        label: const Text("Email"),
+                        isDense: true,
+                        hintText: "Email Anda",
+                        fillColor: Colors.red[100],
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == "") {
+                          return "Email Wajib diisi";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    child: TextFormField(
+                      controller: phoneNumberController,
+                      keyboardType: TextInputType.name,
+                      textInputAction: TextInputAction.search,
+                      decoration: InputDecoration(
+                        suffixIcon: const Icon(Icons.phone_android_rounded),
+                        label: const Text("No Hp"),
+                        isDense: true,
+                        hintText: "No Hp anda",
+                        fillColor: Colors.red[100],
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              if (_formKey.currentState!.validate()) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text("Success"),
+                  ),
+                );
+              }
+            },
+            child: Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              width: double.infinity,
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    16,
+                  ),
+                  color: Colors.blue),
+              child: const Center(
+                child: Text(
+                  "Simpan",
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+        ],
+      ),
+    );
+  }
+}
